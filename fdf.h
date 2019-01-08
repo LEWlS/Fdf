bfebca3bf96eb95bea35e6411ee8ba6e4b0cd883
Lewis BONNETETE <lbonnete@e1r11p20.42.fr>
2019-01-08 18:22:03 +0100

    messing aroud

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: lbonnete <lbonnete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 16:54:05 by lbonnete          #+#    #+#             */
-/*   Updated: 2019/01/08 16:32:02 by lbonnete         ###   ########.fr       */
+/*   Updated: 2019/01/08 18:18:48 by lbonnete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,26 @@
 # include "get_next_line/get_next_line.h"
 # include "mlx.h"
 
-typedef struct  s_info
+typedef struct  s_key_info
 {
     void    *ptr;
     void    *win;
     int     x;
     int     y;
     int     z;
+    int     color;
 }               t_info;
+typedef struct  s_mouse_info
+{
+    void    *ptr;
+    void    *win;
+    int     x;
+    int     y;
+    int     z;
+    int     color;
+}               t_mouse_info;
+
+void	put_new_square(t_info *try, int key, int size);
+void	put_square(t_info *try, int key, int size, int color);
 
 #endif
--- main.c
+++ main.c
@@ -6,73 +6,26 @@
 /*   By: lbonnete <lbonnete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 15:40:23 by lbonnete          #+#    #+#             */
-/*   Updated: 2019/01/08 16:58:32 by lbonnete         ###   ########.fr       */
+/*   Updated: 2019/01/08 18:21:52 by lbonnete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	put_square_4(t_info *try , int key)
-{
-	int x;
-	int y;
-
-	x = try->x;
-	while (x < try->x + 4)
-	{
-		y = try->y;
-		while(y < try->y + 4)
-		{
-			mlx_pixel_put(try->ptr, try->win, x, y, 123);
-			y++;
-		}
-		x++;
-	}
-	if (key == 126)
-		try->x -= 4;
-	if (key == 124)
-		try->y += 4;
-	if (key == 125)
-		try->x += 4;
-	if (key == 123)
-		try->y -= 4;
-}
-
-void	Rput_square_4(t_info *try , int key)
-{
-	int x;
-	int y;
-
-	x = try->x;
-	while (x < try->x + 4)
-	{
-		y = try->y;
-		while(y < try->y + 4)
-		{
-			mlx_pixel_put(try->ptr, try->win, x, y, 456);
-			y++;
-		}
-		x++;
-	}
-	if (key == 126)
-		try->x -= 4;
-	if (key == 124)
-		try->y += 4;
-	if (key == 125)
-		try->x += 4;
-	if (key == 123)
-		try->y -= 4;
-}
-
 int		deal_key(int key,	t_info *try)
 {
 	ft_putnbr(key);
+	ft_putendl("");
 	if (key == 53)
 		mlx_destroy_window (try->ptr, try->win);
-	if (123 >= key && key <= 126)
-		Rput_square_4(try, key);
+	else
+		put_new_square(try, key, 20);
 	return (0);
 	
+}
+int		deal_mouse(int button, int x, int y, void *param)
+{
+
 }
 
 int		main(void)
@@ -81,12 +34,10 @@ int		main(void)
 
 	try.x = 500;
 	try.y = 500;
+	try.color = 123;
 	try.ptr = mlx_init();
 	try.win = mlx_new_window(try.ptr, 1000, 1000, "New");
-	put_square_4(&try, 0);
-	try.x -= 100;
-	try.y -= 100;
-	put_square_4(&try, 0);
+	mlx_mouse_hook(try.win, deal_mouse, &try);
 	mlx_key_hook (try.win, deal_key, &try);
 	mlx_loop(try.ptr);
 	return (0);
